c5663f525c946df5dd3e45274c8d63c677b95c13
MatheusKevin <kevin.pelegrini@outlook.com>
2019-08-23 22:43:06 +0000

    Moeda de origem usada na API

--- main.js
+++ main.js
@@ -1,12 +1,16 @@
-function chamarAPI() {
-	const endpoint = "https://api.exchangeratesapi.io/latest";
-	$.get(endpoint, callback);
-}
-
 function callback(resultado) {
 	var sMoedaDestino = $("#moeda_destino").val();
 	var fvalor = resultado.rates[sMoedaDestino];
 	
 	$("#valor_destino").text(fvalor);
 	$("#moeda_destino_cel").text(sMoedaDestino);
+}
+
+function chamarAPI() {
+	var endpoint = "https://api.exchangeratesapi.io/latest";
+	var oParametros = {
+		base: $("#moeda_origem").val()
+	};
+	
+	$.get(endpoint, oParametros, callback);
 }
